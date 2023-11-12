bffa568eca6de342dbfef0f9a7a609cf5e6ea406
diegoccrs <caceresdiego2607@gmail.com>
2023-11-11 20:29:26 -0400

    Creacion de las primeras clases

---
+++ src/proyecto2/Nodo.java
@@ -0,0 +1,29 @@
+package proyecto2;
+
+public class Nodo<T> {
+    private T data;
+    private Nodo<T> pNext;
+
+    public Nodo(T data){
+        this.data = data;
+        this.pNext = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Nodo<T> getpNext() {
+        return pNext;
+    }
+
+    public void setpNext(Nodo<T> pNext) {
+        this.pNext = pNext;
+    }
+    
+    
+}
